ccc5ac7fcf7384515a8865fbce3de1ed1dc0966e
John Shea <sheajohnh@gmail.com>
2019-03-04 23:57:38 -0500

    Add pixel art maker code for creating and coloring grid

--- designs.js
+++ designs.js
@@ -1,10 +1,48 @@
 // Select color input
+var boxColor = '#000000';
+
 // Select size input
+var gridWidth = 1;
+var gridHeight = 1;
 
-// When size is submitted by the user, call makeGrid()
+document.querySelector('#sizePicker').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-function makeGrid() {
+  gridHeight = event.target.elements[0].valueAsNumber;
+  gridWidth = event.target.elements[1].valueAsNumber;
 
-// Your code goes here!
+  makeGrid(gridWidth, gridHeight);
+});
 
+document.querySelector('#colorPicker').addEventListener('change', function(event) {
+    boxColor = event.target.value;
+});
+
+function colorBox() {
+  this.style.backgroundColor = boxColor;
+}
+
+function makeGrid(width, height) {
+
+  // Find table element
+  var t = document.querySelector('#pixelCanvas');
+
+  // Remove existing table rows and cells
+  while (t.hasChildNodes()) {
+    t.removeChild(t.firstChild);
+  }
+
+  // Create grid
+  for (let y = 0; y < height; y++) {
+    var tr = document.createElement('tr');
+    for(let x = 0; x < width; x++) {
+      var td = document.createElement('td');
+
+      // Add listener to grid cell
+      td.addEventListener('click', colorBox);
+
+      tr.appendChild(td);
+    }
+    t.appendChild(tr);
+  }
 }
